3747d20b5d0066a3a1bea09c2adb17a049c008d6
ajit51 <ajitk433@gmail.com>
2024-03-05 00:25:12 +0530

    spring boot 3.2.2 configuration

--- src/main/java/com/kafka/api/KafkaPublisherApplication.java
+++ src/main/java/com/kafka/api/KafkaPublisherApplication.java
@@ -13,28 +13,12 @@ import com.kafka.api.model.User;
 @SpringBootApplication
 @RestController
 public class KafkaPublisherApplication {
-	
-	@Autowired
-	private KafkaTemplate<String, Object> template;
-	
-	private static final String TOPIC_NAME = "TestTopic";
-	
-	
-	@GetMapping("/publish/{name}")
-	public String publisgMessage(@PathVariable String name) {
-		template.send(TOPIC_NAME, "Hi "+name+" Welcome to Kafka");
-		return "Data Published";
-	}
-	
-	@GetMapping("/publishJson")
-	public String publisgMessage() {
-		User user = new User(1212, "ABC", new String[] {"Banblore", "Pune", "Mumbai"});
-		template.send(TOPIC_NAME, user);
-		return "Json Data Published";
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaPublisherApplication.class, args);
-	}
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPublisherApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/kafka/api/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package com.kafka.api.constant;
+
+public class AppConstant {
+
+    public static final String TOPIC_NAME = "javatechie-demo2";
+    public static final String GROUP_ID = "group_id";
+}
